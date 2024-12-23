e8aba31b31f41019ad7ffbf412cf60e2652012ef
zhaoxu <zhaoxu.email@gmail.com>
2024-12-23 14:14:31 +0800

    options page

--- src/app/pages/options/options.component.ts
+++ src/app/pages/options/options.component.ts
@@ -1,60 +1,79 @@
 import {Component, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {NzButtonComponent} from 'ng-zorro-antd/button';
-import {NzCardComponent} from 'ng-zorro-antd/card';
-import {NzCheckboxComponent} from 'ng-zorro-antd/checkbox';
-import {NzIconDirective} from 'ng-zorro-antd/icon';
-import {NzInputDirective} from 'ng-zorro-antd/input';
-import {NzTableComponent} from 'ng-zorro-antd/table';
+import {NzButtonModule} from 'ng-zorro-antd/button';
+import {NzCardModule} from 'ng-zorro-antd/card';
+import {NzCheckboxModule} from 'ng-zorro-antd/checkbox';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzInputModule} from 'ng-zorro-antd/input';
+import {NzTableModule} from 'ng-zorro-antd/table';
 import {Rule} from '../rule';
 
 @Component({
     selector: 'app-options',
     templateUrl: './options.component.html',
-    styleUrl: './options.component.css',
+    styleUrl: './options.component.scss',
     imports: [
-        NzTableComponent,
-        NzCardComponent,
-        NzInputDirective,
         FormsModule,
-        NzButtonComponent,
-        NzIconDirective,
-        NzCheckboxComponent
+        NzTableModule,
+        NzCardModule,
+        NzInputModule,
+        NzButtonModule,
+        NzIconModule,
+        NzCheckboxModule
     ]
 })
 export class OptionsComponent {
     rules = signal<Rule[]>([]);
 
     constructor() {
-        if (chrome?.storage) {
-            this.loadRules();
-        } else {
-            this.devTest();
-        }
+        this.loadRules();
     }
 
     loadRules() {
-        chrome.storage.local.get(['rules']).then(item => {
-            let rules = item['rules'] as Rule[];
-            if (!rules) {
-                let v2Rules = localStorage.getItem('rules');
-                if (v2Rules) {
-                    rules = JSON.parse(v2Rules) as Rule[];
+        if (chrome?.storage) {
+            chrome.storage.local.get(['rules']).then(item => {
+                let rules = item['rules'] as Rule[];
+                if (!rules) {
+                    let v2Rules = localStorage.getItem('rules');
+                    if (v2Rules) {
+                        rules = JSON.parse(v2Rules) as Rule[];
+                    }
+                }
+                if (rules) {
+                    this.rules.set(rules);
+                }
+            });
+        } else {
+            let v2Rules = localStorage.getItem('rules');
+            if (v2Rules) {
+                let rules = JSON.parse(v2Rules) as Rule[];
+                if (rules) {
+                    this.rules.set(rules);
                 }
             }
-            if (rules) {
-                this.rules.set(rules);
-            }
-        })
+        }
     }
 
-    devTest() {
-        let v2Rules = localStorage.getItem('rules');
-        if (v2Rules) {
-            let rules = JSON.parse(v2Rules) as Rule[];
-            if (rules) {
-                this.rules.set(rules);
-            }
+    addRule() {
+        this.rules.update(rules => [...rules, {value: '', path: '', disabled: false}]);
+    }
+
+    deleteRule(rule: Rule) {
+        this.rules.update(rules => rules.filter(r => r !== rule));
+        if (this.rules().length == 0) {
+            this.addRule();
+        }
+    }
+
+    saveRules() {
+        let rules = [...this.rules().filter(r => r.value || r.path || r.disabled)];
+        if (chrome?.storage) {
+            chrome.storage.local.set({rules}).then(() => {
+                this.loadRules();
+            });
+        } else {
+            localStorage.setItem('rules', JSON.stringify(rules));
+            this.loadRules();
         }
     }
 }
